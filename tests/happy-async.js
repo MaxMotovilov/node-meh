7209c36896787fcd2d4760d1e64cab9ecb88f14d
Max <max@motovilov.com>
2017-06-01 10:09:25 -0500

    Async tests now use Cort

--- tests/happy-async.js
+++ tests/happy-async.js
@@ -1,196 +1,192 @@
 const 	meh = require( "../index" ),
-		assert = require( "assert" );
+        cort = require( "cort-unit/nodeunit" ),
+        assert = require( "assert" );
 
-function later( fn ) {
-	setImmediate( fn )
-}
+exports.minimal = cort( function( test ) {	
+    const 	sink = meh.source()
+                      .sink( handler ),
+            request = sink.request( "GET", "foo" );
 
-exports.minimal = function( test ) {	
-	const 	sink = meh.source()
-				      .sink( handler ),
-			request = sink.request( "GET", "foo" );
+    request.sink( "response", response => {
+        assert.equal( response.status, 200 );
+        assert( response.headers );
+        
+        response.sink( "body", body => {
+            assert.equal( body, "Hello world!" );
+            test.done();
+        } );
+    } );
 
-	request.sink( "response", response => {
-		assert.equal( response.status, 200 );
-		assert( response.headers );
-		
-		response.sink( "body", body => {
-			assert.equal( body, "Hello world!" );
-			test.done();
-		} );
-	} );
+    test.later( () => sink.submit( request ) );
 
-	later( () => sink.submit( request ) );
+    function handler( request ) {
+        assert.equal( request.method, "GET" );
+        assert.equal( request.url, "foo" );
+        assert( request.headers );
+        
+        var response;
 
-	function handler( request ) {
-		assert.equal( request.method, "GET" );
-		assert.equal( request.url, "foo" );
-		assert( request.headers );
-		
-		var response;
+        test.later( () => (response = request.respond( 200 )) )
+            .later( () => response.body( "Hello world!" ) );
+    }
+} );
 
-		later( () => (response = request.respond( 200 )) );
-		later( () => response.body( "Hello world!" ) );
-	}
-}
+exports.middleware = cort( function( test ) {
+    const 	sink = meh.source()
+                      .pipe( middle )
+                      .sink( handler ),
+            request = sink.request( "GET", "foo" );
 
-exports.middleware = function( test ) {
-	const 	sink = meh.source()
-					  .pipe( middle )
-				      .sink( handler ),
-			request = sink.request( "GET", "foo" );
+    request.sink( "response", response => {
+        assert.equal( response.status, 200 );
+        assert( response.headers );
+        
+        response.sink( "body", body => {
+            assert.equal( body, "Hello middleware world!" );
+            debugger;
+            test.done();
+        } );
+    } );
 
-	request.sink( "response", response => {
-		assert.equal( response.status, 200 );
-		assert( response.headers );
-		
-		response.sink( "body", body => {
-			assert.equal( body, "Hello middleware world!" );
-			test.done();
-		} );
-	} );
+    test.later( () => sink.submit( request ) );
 
-	later( () => sink.submit( request ) );
+    function middle( request, _, next ) {
+        request.headers["x-middleware"] = "bar";
 
-	function middle( request, _, next ) {
-		request.headers["x-middleware"] = "bar";
+        request.pipe( "response", (response, next) => {
 
-		request.pipe( "response", (response, next) => {
-			later( () => response.pipe( "body", (body, next) => {
-				assert.equal( body, "Hello world!" );
-				later( () => next( "Hello middleware world!" ) );
-			} ) );
+            test.later( "response.pipe", () => response.pipe( "body", (body, next) => {
+                assert.equal( body, "Hello world!" );
+                test.later( () => next( "Hello middleware world!" ) );
+            } ) )
+                .later( "response.next", next );
+        } );
 
-			later( next );
-		} );
+        test.later( "middle.next", next );
+    }
 
-		later( next );
-	}
+    function handler( request ) {
+        assert.equal( request.method, "GET" );
+        assert.equal( request.url, "foo" );
+        assert( request.headers );
+        assert.equal( request.headers["x-middleware"], "bar" );
+    
+        var response;
 
-	function handler( request ) {
-		assert.equal( request.method, "GET" );
-		assert.equal( request.url, "foo" );
-		assert( request.headers );
-		assert.equal( request.headers["x-middleware"], "bar" );
-	
-		var response;
+        test.later( () => (response = request.respond( 200 )) )
+            .later( () => response.body( "Hello world!" ) );
+    }
+} );
 
-		later( () => (response = request.respond( 200 )) );
-		later( () => response.body( "Hello world!" ) );
-	}
-}
+exports.middlewareRender = cort( function( test ) {
+    const 	sink = meh.source()
+                      .pipe( middle )
+                      .sink( handler ),
+            request = sink.request( "GET", "foo" ),
 
-exports.middlewareRender = function( test ) {
-	const 	sink = meh.source()
-					  .pipe( middle )
-				      .sink( handler ),
-			request = sink.request( "GET", "foo" ),
+            response_body = { foo: "bar", bar: 1 };
 
-			response_body = { foo: "bar", bar: 1 };
+    request.sink( "response", response => {
+        assert.equal( response.status, 200 );
+        assert( response.headers );
+        
+        var	output = "";
 
-	request.sink( "response", response => {
-		assert.equal( response.status, 200 );
-		assert( response.headers );
-		
-		var	output = "";
+        response.sink( "data", chunk => {
+            if( chunk )
+                output += chunk.toString();
+            else {				
+                assert.deepEqual( JSON.parse( output ), response_body );
+                test.done();
+            }
+        } );
+    } );
 
-		response.sink( "data", chunk => {
-			if( chunk )
-				output += chunk.toString();
-			else {				
-				assert.deepEqual( JSON.parse( output ), response_body );
-				test.done();
-			}
-		} );
-	} );
+    test.later( () => sink.submit( request ) );
 
-	later( () => sink.submit( request ) );
+    function middle( request, _, next ) {
+        request.headers["x-middleware"] = "bar";
 
-	function middle( request, _, next ) {
-		request.headers["x-middleware"] = "bar";
+        request.pipe( "response", (response, next) => {
+            test.later( "response.sink", () => response.sink( "body", body => {
+                const response_data = JSON.stringify( body );
+                test.later( () => response.data( new Buffer( response_data.substr( 0, 10 ) ) ) )
+                    .later( () => response.data( new Buffer( response_data.substr( 10 ) ) ) )
+                    .later( () => response.data() );
+            } ) )
+                .later( "response.next", next );
+        } );
 
-		request.pipe( "response", (response, next) => {
-			later( () => response.sink( "body", body => {
-				const response_data = JSON.stringify( body );
-				later( () => response.data( new Buffer( response_data.substr( 0, 10 ) ) ) );
-				later( () => response.data( new Buffer( response_data.substr( 10 ) ) ) );
-				later( () => response.data() );
-			} ) );
+        test.later( "middle.next", next );
+    }
 
-			later( next );
-		} );
+    function handler( request ) {
+        assert.equal( request.method, "GET" );
+        assert.equal( request.url, "foo" );
+        assert( request.headers );
+        assert.equal( request.headers["x-middleware"], "bar" );
 
-		later( next );
-	}
+        var response;
 
-	function handler( request ) {
-		assert.equal( request.method, "GET" );
-		assert.equal( request.url, "foo" );
-		assert( request.headers );
-		assert.equal( request.headers["x-middleware"], "bar" );
+        test.later( () => (response = request.respond( 200 )) )
+            .later( () => response.body( response_body ) );
+    }
+} );
 
-		var response;
+exports.middlewareParse = cort( function( test ) {
+    const 	sink = meh.source()
+                      .pipe( middle )
+                      .sink( handler ),
+            request = sink.request( "POST", "foo" ),
 
-		later( () => (response = request.respond( 200 )) );
-		later( () => response.body( response_body ) );
-	}
-}
+            request_body = { foo: "bar", bar: 1 };
 
-exports.middlewareParse = function( test ) {
-	const 	sink = meh.source()
-					  .pipe( middle )
-				      .sink( handler ),
-			request = sink.request( "POST", "foo" ),
+    request.sink( "response", response => {
+        assert.equal( response.status, 200 );
+        assert( response.headers );
+        
+        response.sink( "body", body => {
+            assert.equal( body, "Hello world!" );
+            test.done();
+        } );
+    } );
 
-			request_body = { foo: "bar", bar: 1 };
+    const request_data = JSON.stringify( request_body );
 
-	request.sink( "response", response => {
-		assert.equal( response.status, 200 );
-		assert( response.headers );
-		
-		response.sink( "body", body => {
-			assert.equal( body, "Hello world!" );
-			test.done();
-		} );
-	} );
+    test.later( () => sink.submit( request ) )
+        .later( () => request.data( new Buffer( request_data.substr( 0, 10 ) ) ) )
+        .later( () => request.data( new Buffer( request_data.substr( 10 ) ) ) )
+        .later( () => request.data() );
 
-	later( () => sink.submit( request ) );
+    function middle( request, _, next ) {
+        request.headers["x-middleware"] = "bar";
 
-	const request_data = JSON.stringify( request_body );
+        var input = "";
 
-	later( () => request.data( new Buffer( request_data.substr( 0, 10 ) ) ) );
-	later( () => request.data( new Buffer( request_data.substr( 10 ) ) ) );
-	later( () => request.data() );
+        request.sink( "data", chunk => {
+            if( chunk )
+                input += chunk.toString();
+            else
+                request.body( JSON.parse( input ) );
+        } );
 
-	function middle( request, _, next ) {
-		request.headers["x-middleware"] = "bar";
+        test.later( "middle.next", next );
+    }
 
-		var input = "";
+    function handler( request ) {
+        assert.equal( request.method, "POST" );
+        assert.equal( request.url, "foo" );
+        assert( request.headers );
+        assert.equal( request.headers["x-middleware"], "bar" );
+    
+        request.sink( "body", body => {
+            assert.deepEqual( body, request_body );
 
-		request.sink( "data", chunk => {
-			if( chunk )
-				input += chunk.toString();
-			else
-				request.body( JSON.parse( input ) );
-		} );
+            var response;
 
-		later( next );
-	}
-
-	function handler( request ) {
-		assert.equal( request.method, "POST" );
-		assert.equal( request.url, "foo" );
-		assert( request.headers );
-		assert.equal( request.headers["x-middleware"], "bar" );
-	
-		request.sink( "body", body => {
-			assert.deepEqual( body, request_body );
-
-			var response;
-
-			later( () => (response = request.respond( 200 )) );
-			later( () => response.body( "Hello world!" ) );
-		} );
-	}
-}
+            test.later( () => (response = request.respond( 200 )) )
+                .later( () => response.body( "Hello world!" ) );
+        } );
+    }
+} );
 
